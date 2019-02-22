6a34f1d84a0330f9227bf3cd30acb93d1c4016fe
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-02-22 15:10:03 -0800

    basic html formatting, declared global variables in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var userGuess;
+var winCount = 0;
+var lossCount = 0;
+var wordList = [];
+var word = [];
+var hiddenWord = [];
+var guesses = [];
+
+
+document.onkeyup = function (event) {
+    userGuess = event.key;
+}
